4df1f9f4a8866cadd8f3116f1e41a9d7f5d6849a
Zaargh <gitlab.com@dumpsite.xyz>
2024-07-02 20:54:26 +0300

    chore: terraform fmt

--- clouds/aws/tf_backend/s3.tf
+++ clouds/aws/tf_backend/s3.tf
@@ -13,7 +13,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "state_encryption"
   bucket = aws_s3_bucket.terraform_state.bucket
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm     = "AES256"
+      sse_algorithm = "AES256"
     }
   }
 }
