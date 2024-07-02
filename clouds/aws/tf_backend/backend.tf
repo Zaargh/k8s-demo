9bdc752810d0b8c8ee0a312178d75c23703aff94
Zaargh <gitlab.com@dumpsite.xyz>
2024-07-02 20:38:29 +0300

    feat: use the tf backend

--- clouds/aws/tf_backend/backend.tf
+++ clouds/aws/tf_backend/backend.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "<ENTER-YOUR-BUCKET-NAME-HERE>"
-#     key            = "aws/tf_backend/terraform.tfstate"
-#     region         = "eu-central-1"
-#   #  dynamodb_table = "ja-tflock"  # I'm working on this project on my own, so it isn't strictly necessary.
-#     encrypt        = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "ja-tfstate-20240702173626741400000001"
+    key            = "aws/tf_backend/terraform.tfstate"
+    region         = "eu-central-1"
+  #  dynamodb_table = "ja-tflock"  # I'm working on this project on my own, so it isn't strictly necessary.
+    encrypt        = true
+  }
+}
